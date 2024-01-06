b22175ea9b5ee9f682ad20001639c92bdee6520a
guicunhadev <guilhermealmeida2008@hotmail.com>
2024-01-06 10:40:31 -0300

    Base- site Done

---
+++ main.js
@@ -0,0 +1,31 @@
+// ------------Button Menu Cellphone------------
+
+const menuBtn = document.getElementById("menu-btn")
+const navLinks = document.getElementById("nav-links")
+const menuBtnIcon = menuBtn.querySelector("i")
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open")
+
+    const isOpen = navLinks.classList.contains("open")
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+
+// --------Swiper Customer----------
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
