2d9fe8863f93722ef3b5f91dcd38b09497306bce
guicunhadev <guilhermealmeida2008@hotmail.com>
2024-01-29 13:45:06 -0300

    site almost ready

--- main.js
+++ main.js
@@ -34,23 +34,82 @@ navLinks.addEventListener("click", (e) => {
 });
 
 
-// --------Swiper Customer----------
+// -- Initialize Swiper --
 
-const swiper = new Swiper(".swiper", {
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+      const progressCircle = document.querySelector(".autoplay-progress svg");
+      const progressContent = document.querySelector(".autoplay-progress span");
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 10000, //Time SLIDER
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        },
+        on: {
+          autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+          }
+        }
+      });
+
+
+// ----------------------------Send Email------------------
+
+const form = document.querySelector('form');
+const name = document.getElementById("fullName");
+const phone = document.getElementById("phone");
+
+function sendEmail() {
+const bodyMessage = `Nome Completo: ${fullName.value}<br> 
+Telefone: ${phone.value}<br>`;
+
+  Email.send({
+    SecureToken: "7f571-64cf-4f7e-aa72-d8d2b3fc2a3c",
+    Host : "smtp.elasticemail.com",
+    Username : "guilhermealmeida2008@hotmail.com",
+    Password : "471929B9579A1DB048BD10E51EC79496B179",
+    To : 'guilhermealmeida2008@hotmail.com',
+    From : "guilhermealmeida2008@hotmail.com",
+    Subject : "Você Recebeu o contato de um cliente",
+    Body : bodyMessage
+}).then(
+  message => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Telefone Enviado!",
+        text: "Entraremos em contato o mais breve possivel!",
+        icon: "success"
+      });
+    }
+  }
+);
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  sendEmail();
+
+  form.reset();
+  return false;
 });
 
-// -------------TESTE SCROLL NAVBAR---------
+// ------------Galery Magnific Popup--------------
 
-
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0)
-});
